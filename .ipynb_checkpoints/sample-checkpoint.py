ace258503f06ad877d8e3025042f7caea00d9978
Sincere Miranda <sincerewx17@gmail.com>
2023-10-03 22:09:20 +0000

    updates

---
+++ .ipynb_checkpoints/sample-checkpoint.py
@@ -0,0 +1,6 @@
+"""This is a text file that contains a sample Python script"""
+print ("Hello, Python learners!")
+a = 2
+b = 8
+
+# Add a line to this file that calculates the sum of a and b and prints out the result to the screen.
